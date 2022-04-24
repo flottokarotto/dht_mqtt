4857fed5280faa76ef2dbad4882481e58423591a
Florian Fischer <flottokarotto@gmail.com>
2022-04-24 13:37:56 +0200

    first working rev

---
+++ README.md
@@ -0,0 +1,15 @@
+# Description
+Read temperature/humidity from DHT Sensor using pigpio and send the results to a mqtt broker.
+Intended to use with Home Assistant.
+
+The Broker url and the topics are hard coded. 
+
+# Installation
+- pip install pigpio-dht
+- pip install paho-mqtt
+- sudo apt install pigpio
+
+# Usage 
+Modify the GPIO number, MQTT URL and MQTT topic to your need and run it with cron or systemd timer.
+
+
--- main.py
+++ main.py
@@ -1,50 +1,53 @@
-import paho.mqtt.client as paho
+#!/usr/bin/env python3
+import sys
 import json
-
-from time import sleep
+import paho.mqtt.client as paho
 from pigpio_dht import DHT22
 
-if __name__ == '__main__':
 
-    client = paho.Client()
-    s = DHT22(4)
-    result = s.read()
-    # client.tls_set('root.ca', certfile='c1.crt', keyfile='c1.key')
-    client.connect("homeassistant", 1883, 60)
+client = paho.Client()
+s = DHT22(4)
+result = s.read()
+# client.tls_set('root.ca', certfile='c1.crt', keyfile='c1.key')
+client.connect("homeassistant", 1883, 60)
 
-    config_message_temperature = {
-        "device_class": "temperature",
-        "name": "Attic_Temperature",
-        "state_topic": "homeassistant/sensor/attic/state",
-        "unit_of_measurement": "°C",
-        "value_template":
-            "{{ value_json.temperature}}"
-    }
+config_message_temperature = {
+    "device_class": "temperature",
+    "name": "Attic_Temperature",
+    "state_topic": "homeassistant/sensor/attic/state",
+    "unit_of_measurement": "°C",
+    "value_template":
+        "{{ value_json.temperature}}"
+}
 
-    config_message_humidity = {
-        "device_class": "humidity",
-        "name": "Attic_Humidity",
-        "state_topic": "homeassistant/sensor/attic/state",
-        "unit_of_measurement": "%",
-        "value_template":
-            "{{ value_json.humidity}}"
-    }
+config_message_humidity = {
+    "device_class": "humidity",
+    "name": "Attic_Humidity",
+    "state_topic": "homeassistant/sensor/attic/state",
+    "unit_of_measurement": "%",
+    "value_template":
+        "{{ value_json.humidity}}"
+}
 
-    client.publish("homeassistant/sensor/atticT/config", json.dumps(config_message_temperature), retain=True)
-    client.publish("homeassistant/sensor/atticH/config", json.dumps(config_message_humidity), retain=True)
+client.publish("homeassistant/sensor/atticT/config",
+               json.dumps(config_message_temperature), retain=True)
+client.publish("homeassistant/sensor/atticH/config",
+               json.dumps(config_message_humidity), retain=True)
 
-    while True:
-        try:
-            status = result[2]
-            print(status)
-            temperature = result[3]
-            humidity = result[4]
-            print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-            client.publish("homeassistant/sensor/attic/state", json.dumps({"temperature": temperature, "humidity": humidity}))
-        except Exception as error:
-            print("Failed to retrieve data from sensor")
-            print(error.args[0])
-        sleep(1)
+try:
 
-
-
+    if result["valid"]:
+        temperature = result["temp_c"]
+        humidity = result["humidity"]
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        client.publish("homeassistant/sensor/attic/state",
+                       json.dumps({"temperature": temperature, "humidity": humidity}))
+        sys.exit(0)
+    else:
+        print("Failed to retrieve data from sensor")
+        print("Data invalid")
+        sys.exit(-1)
+except Exception as error:
+    print("Failed to retrieve data from sensor")
+    print(error.args[0])
+    sys.exit(-1)
